4decd748eae21cb49d3d15e361e2266d4b4b0e28
Olladi <aleksandr.zuev01@mail.ru>
2023-08-30 03:03:30 +0300

    Второй коммит

---
+++ main.js
@@ -0,0 +1,69 @@
+//  console.log("hello World");
+//  console.log(123);
+//  console.log("hello"+" World");
+
+
+
+// задание 1
+// let a = 1; // номер месяца, в котором родился
+// console.log(a);
+
+// задание 2
+console.log("Добро "+"пожаловать "+"на курс "+"Марат");
+
+//задание 3
+// alert('25.08.2023');
+
+//задание4
+document.getElementById ("one").innerHTML = "Hello World"
+
+//задание 5
+
+// let a =1/0;
+// console.log(a);
+// let a = Infinity;
+// console.log(a);
+
+// let a =1/"аниме";
+// console.log(a);
+// let a = 1;
+// console.log(NaN**0);
+
+
+// console.log(13567893456712357855269864352815553584);
+// console.log(13567893456712357855269864352815553584n);
+
+
+// let a ='я';
+// let b ="люблю";
+// c =`Майнкрафт ${1+33}`;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let check = true;
+// let oncheck = false;
+// console.log(1<2);
+
+// let m = null;
+// console.log(m);
+
+// let ssd;
+// console.log(ssd);
+
+// console.log(typeof undefined);
+// console.log(typeof 12);
+// console.log(typeof 12n);
+// console.log(typeof false);
+// console.log(typeof 'fghggf');
+// console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
